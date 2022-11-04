3887cfacd37500b77aa73358f95a194afa9bc79d
Luis Angel Santos <santosangel748@gmail.com>
2022-11-04 17:56:28 -0600

    Definiendo los servicios REST

--- server/routes.js
+++ server/routes.js
@@ -40,6 +40,46 @@ router.post('/', function (req, res) {
   });
 });
 
+// Almacenar la suscripción
+router.post('/subscribe', (req, res) => {
 
+
+  const suscripcion = req.body;
+
+
+  push.addSubscription(suscripcion);
+
+
+  res.json('subscribe');
+
+});
+
+// Almacenar la suscripción
+router.get('/key', (req, res) => {
+
+  const key = push.getKey();
+
+
+  res.send(key);
+
+});
+
+// Envar una notificación PUSH a las personas
+// que nosotros queramos
+// ES ALGO que se controla del lado del server
+router.post('/push', (req, res) => {
+
+  const post = {
+      titulo: req.body.titulo,
+      cuerpo: req.body.cuerpo,
+      usuario: req.body.usuario
+  };
+
+
+  push.sendPush(post);
+
+  res.json(post);
+
+});
 
 module.exports = router;
